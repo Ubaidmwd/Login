e777e8939a403e053638c82951a46083d671c265
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-18 04:23:42 -0800

    adding

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,20 @@ const express =require('express')
 require('dotenv').config({ path: "backend\config.env" });
 
 const app=express()
-// const PORT=4000
+const mongoose=require('mongoose')
+
+// const PORT=4000 1XfmMFfAK8gMBUlV
+// mongodb+srv://ubaid:1XfmMFfAK8gMBUlV@cluster0.owxo0v7.mongodb.net/?retryWrites=true&w=majority
+
+mongoose.set('strictQuery', false);
+
+const url ='mongodb://localhost:27017/Login'
+mongoose.connect(url).then(() => {
+    console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});
+
 
 app.get('/',(req,res)=>{
 
@@ -10,9 +23,6 @@ app.get('/',(req,res)=>{
 
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log('port is strating on Port',process.env.PORT)
-})
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT || 4000}`);
   });
