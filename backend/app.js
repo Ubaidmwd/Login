a8ca136204810ee94f222885a99765bd139969fa
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-18 22:29:55 -0800

    hashing

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,15 @@ const express =require('express')
 require('dotenv').config({ path: "backend\config.env" });
 
 const app=express()
+app.use(express.json())
 const mongoose=require('mongoose')
+const User=require('./models/user')
+const bcrypt=require('bcryptjs')
 
-// const PORT=4000 1XfmMFfAK8gMBUlV
-// mongodb+srv://ubaid:1XfmMFfAK8gMBUlV@cluster0.owxo0v7.mongodb.net/?retryWrites=true&w=majority
-
+//////////////////////////////////////////////////////////////////////////////////////
+// const PORT=4000 1XfmMFfAK8gMBUlV                                              //   
+// mongodb+srv://ubaid:1XfmMFfAK8gMBUlV@cluster0.owxo0v7.mongodb.net/?           // retryWrites=true&w=majority
+//////////////////////////////////////////////////////////////////////////////////////
 mongoose.set('strictQuery', false);
 
 const url ='mongodb://localhost:27017/Login'
@@ -20,6 +24,36 @@ mongoose.connect(url).then(() => {
 app.get('/',(req,res)=>{
 
     res.json({message:"Hello to node js app"})
+
+})
+
+app.post('/signup',async (req,res)=>{
+
+   
+    try{
+        const {email,password}=req.body
+        console.log(req.body)
+        if(!email || !password){
+            return res.status(422).json({error:"please add all fields"})
+        }
+        
+        const user = await User.findOne({email})
+        if(user){
+            return res.status(422).json({error:"Email is Already Exsist"})
+    
+        }
+        const hashedPassword=await bcrypt.hash(password,12)
+        await new User({
+            email,
+            password:hashedPassword
+        }).save()
+        res.status(200).json({message:"Singup Sucessful Now Login"})
+
+    }catch(error){
+        console.log(error)
+
+    }
+    
 
 })
 
