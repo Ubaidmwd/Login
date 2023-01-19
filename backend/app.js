3bb44a90a139158e883deb6a14a6b68d8e16e78e
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-18 23:39:01 -0800

    sequence

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,18 @@
-const express = require('express')
 require('dotenv').config({ path: "backend\config.env" });
+const express = require('express')
 const app = express()
 app.use(express.json())
 const mongoose = require('mongoose')
 const User = require('./models/user')
 const bcrypt = require('bcryptjs')
-const jwt=require('jsonwebtoken')
-const SECRET_KEY='s2r12fwe3324@wevgerv'
+const jwt = require('jsonwebtoken')
+const SECRET_KEY = 's2r12fwe3324@wevgerv'
 
 //////////////////////////////////////////////////////////////////////////////////////
 // const PORT=4000 1XfmMFfAK8gMBUlV                                              //   
 // mongodb+srv://ubaid:1XfmMFfAK8gMBUlV@cluster0.owxo0v7.mongodb.net/?           // retryWrites=true&w=majority
 //////////////////////////////////////////////////////////////////////////////////////
 mongoose.set('strictQuery', false);
-
 const url = 'mongodb://localhost:27017/Login'
 mongoose.connect(url).then(() => {
     console.log("Connected to Database");
@@ -21,11 +20,8 @@ mongoose.connect(url).then(() => {
     console.log("Not Connected to Database ERROR! ", err);
 });
 
-
 app.get('/', (req, res) => {
-
     res.json({ message: "Hello to node js app" })
-
 })
 
 app.post('/signup', async (req, res) => {
@@ -35,11 +31,9 @@ app.post('/signup', async (req, res) => {
         if (!email || !password) {
             return res.status(422).json({ error: "please add all fields" })
         }
-
         const user = await User.findOne({ email })
         if (user) {
             return res.status(404).json({ error: "Email is Already Exsist" })
-
         }
         const hashedPassword = await bcrypt.hash(password, 12)
         await new User({
@@ -47,12 +41,9 @@ app.post('/signup', async (req, res) => {
             password: hashedPassword
         }).save()
         res.status(200).json({ message: "Singup Sucessful Now Login" })
-
     } catch (error) {
         console.log(error)
-
     }
-
 })
 
 app.post('/login', async (req, res) => {
@@ -62,31 +53,22 @@ app.post('/login', async (req, res) => {
         if (!email || !password) {
             return res.status(422).json({ error: "please add all fields" })
         }
-
         const user = await User.findOne({ email })
         if (!user) {
             return res.status(404).json({ error: "Email does not exsist please signup " })
-
         }
-        const match= await bcrypt.compare(password, user.password)
-       if(match){
-        const token= jwt.sign({userid:user._id},SECRET_KEY)
-        res.status(201).json({token})
-
-       }
-       else{
-
-        return res.status(401).json({ error: "email or passoword is incorrect " })
-
-
-       }
-
-    } 
+        const match = await bcrypt.compare(password, user.password)
+        if (match) {
+            const token = jwt.sign({ userid: user._id }, SECRET_KEY)
+            res.status(201).json({ token })
+        }
+        else {
+            return res.status(401).json({ error: "email or passoword is incorrect " })
+        }
+    }
     catch (error) {
         console.log(error)
-
     }
-
 })
 
 app.listen(process.env.PORT || 4000, () => {
