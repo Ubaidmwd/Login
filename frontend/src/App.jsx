a0202defc0bcc5f6d4a6e00e700e8486bd60b07d
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-20 23:58:57 -0800

    counter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, incrementByAmount } from './reducers/counter'
 
 const App = () => {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
   return (
-    <div>App</div>
+    <div>
+      <span style={{ fontSize: "50px", display: "flex", justifyContent: "center" }}>{count}</span>
+      <div>
+
+        <button
+          aria-label="Increment value"
+          style={{ width: "20%", height: "50px" }}
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+
+        <button
+          aria-label="Decrement value"
+          style={{ width: "20%", height: "50px" }}
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <button
+          aria-label="Decrement value"
+          style={{ width: "20%", height: "50px" }}
+          onClick={() => dispatch(incrementByAmount(50))}
+        >
+          incrementByAmount
+        </button>
+      </div>
+    </div>
   )
 }
 
-export default App;
+export default App
--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../reducers/counter";
 
 export const store= configureStore({
-    reducer:{},
+    reducer:{counter:counterSlice},
 
 })
