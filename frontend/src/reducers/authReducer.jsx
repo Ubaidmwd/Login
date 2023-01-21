380e4a8381009523edbfabed5a375d754cd69d7c
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-21 04:38:01 -0800

    fetch apis

--- frontend/src/reducers/authReducer.jsx
+++ frontend/src/reducers/authReducer.jsx
@@ -1,5 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+const fetchapi=async(api,body,token="",method="post")=>{
+    const res= awaitfetch(api,{
+        method:method,
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(body)
+
+      })
+      return await res.json()
+
+}
+
+// First, create the thunk
+const signUp = createAsyncThunk(
+    'users/fetchByIdStatus',
+    async (body) => {
+       const result= fetchapi('/signup',body,method='post')
+       return result;
+      
+    }
+  )
+
 const initialState={
     token:"",
     loading:false,
