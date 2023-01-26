438c556cb877cb372ed3d1fb1ab7487e24a5f601
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-25 22:24:02 -0800

    todo

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import e from 'express'
+import React, { useState } from 'react'
 
 const Todo = () => {
+    const [todo,setTodo]=useState("")
+    const addTodo=()=>{
+        setTodo(e.target.value)
+        console(todo)
+
+    }
     return (
         <>
-                <input type="text" name="" id="" placeholder='Enter Your Todo' />
+            <input type="text" name="" id="" placeholder='Enter Your Todo' value={todo}  />
 
-                <button className='btn' onClick={()=>console.log("Hello")}>Add</button>
-                
-          
-
+            <button className='btn' onClick={addTodo}>Add</button>
         </>
     )
 }
