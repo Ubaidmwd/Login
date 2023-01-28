4c7f1c69c2eb76c9355d991e05d725d0f728413c
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-28 04:45:59 -0800

    change

--- app.js
+++ app.js
@@ -129,10 +129,11 @@ app.delete('/todos/:id',verification,async (req,res)=>{
 })
 
 if(process.env.NODE_ENV=='production'){
-    const path=require('path')
-    app.get('/',(res,req)=>{
-        app.use(express.static(path.resolve(__dirname, "./frontend/build")));
-        res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
+    const path = require('path')
+
+    app.get('/',(req,res)=>{
+        app.use(express.static(path.resolve(__dirname,'frontend','build')))
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
     })
 }
 
